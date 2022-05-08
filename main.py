8ad580566868e9abc24e7f2e41ddc6698842d9cf
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2022-05-08 16:26:41 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,61 @@
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+radio.set_group(69)
+radio.set_transmit_serial_number(True)
+radio.set_transmit_power(7)
+enabled = 0
+
+def on_button_pressed_a():
+    global enabled
+    if enabled == 1:
+        radio.send_value("vote", 1)
+        basic.show_string("A")
+        basic.clear_screen()
+    
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    global enabled
+    if enabled == 1:
+        radio.send_value("vote", 2)
+        basic.show_string("B")
+        basic.clear_screen()
+    
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_pin_pressed_p1():
+    global enabled
+    if enabled == 1:
+        radio.send_value("vote", 3)
+        basic.show_string("C")
+        basic.clear_screen()
+    
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+def on_pin_pressed_p2():
+    global enabled
+    if enabled == 1:
+        radio.send_value("vote", 4)
+        basic.show_string("D")
+        basic.clear_screen()
+    
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_received_value(name, value):
+    global enabled
+    if name == "enabled" and value == 1:
+        enabled = 1
+        
+    if name == "enabled" and value == 0:
+        enabled = 0
+        
+    if name == "ack" and value == control.device_serial_number():
+        
+        basic.show_icon(IconNames.HEART)
+        basic.pause(300)
+        basic.clear_screen()
+radio.on_received_value(on_received_value)
+
+def on_received_number(receivedNumber):
+    basic.show_icon(IconNames.HEART)
+    basic.pause(3000)
+    basic.clear_screen()
+radio.on_received_number(on_received_number)
+print(control.device_serial_number())
---
+++ main.ts
@@ -0,0 +1,65 @@
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+radio.setGroup(69)
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(7)
+let enabled = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (enabled == 1) {
+        radio.sendValue("vote", 1)
+        basic.showString("A")
+        basic.clearScreen()
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (enabled == 1) {
+        radio.sendValue("vote", 2)
+        basic.showString("B")
+        basic.clearScreen()
+    }
+    
+})
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
+    if (enabled == 1) {
+        radio.sendValue("vote", 3)
+        basic.showString("C")
+        basic.clearScreen()
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
+    if (enabled == 1) {
+        radio.sendValue("vote", 4)
+        basic.showString("D")
+        basic.clearScreen()
+    }
+    
+})
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+    if (name == "enabled" && value == 1) {
+        enabled = 1
+    }
+    
+    if (name == "enabled" && value == 0) {
+        enabled = 0
+    }
+    
+    if (name == "ack" && value == control.deviceSerialNumber()) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(300)
+        basic.clearScreen()
+    }
+    
+})
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(3000)
+    basic.clearScreen()
+})
+console.log(control.deviceSerialNumber())
